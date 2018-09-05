3c64db8e571ae9b223b7dbd66e1231b24c6af18c
Kelvin Li <jpkelvinli@gmail.com>
2018-09-04 18:42:00 -0700

    Initial commit.

---
+++ src/StartPage.js
@@ -0,0 +1,12 @@
+import './markups/StartPage.css'
+import tpl from './markups/StartPage.html'
+
+export default {
+  name: 'StarPage',
+  template: tpl,
+  data () {
+    return {
+      msg: 'Welcome to P.Play'
+    }
+  }
+}
